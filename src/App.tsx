ed10d151017834ac64ffd1ce92c268ba8e0618a1
Mikkel <miande@dfds.com>
2024-07-26 23:04:26 +0200

    made it work

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ export const App = () => {
   return (
     <div>
       <div className="bg-green-900 text-white px-[16px] h-[80px] flex items-center">
-        Header
+        Top navigation
       </div>
       <Stepper />
+      <div className="h-[2000px]"></div>
+      <div className="bg-red-300 text-white px-[16px] h-[50px] flex items-center">
+        Footer
+      </div>
     </div>
   );
 };
--- src/stepper/Stepper.tsx
+++ src/stepper/Stepper.tsx
@@ -1,21 +1,50 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const Stepper = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [top, setTop] = useState<number>(0);
+
+  const someRef = useRef<HTMLDivElement>(null);
+
+  const topPosition = someRef && someRef.current?.getBoundingClientRect().top;
+
+  useEffect(() => {
+    const onScroll = () => {
+      setTop(someRef?.current?.getBoundingClientRect().top ?? 0);
+    };
+
+    window.addEventListener("scroll", onScroll);
+  }, []);
+
+  console.log("Top", topPosition);
 
   return (
-    <div className="flex flex-col">
-      <div
-        className="bg-gray-300 p-[16px] relative"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        Stepper open: {isOpen ? "Yes" : "No"}
-      </div>
-      {isOpen && (
-        <div className="bg-red-300 absolute bottom-0 left-0 right-0 h-[200px]">
-          This is open
+    <>
+      <div className="sticky top-0 flex flex-col">
+        <div className="bg-blue-800 h-[100px]">
+          Stepper open: {isOpen ? "Yes" : "No"}
         </div>
-      )}
-    </div>
+        <div
+          onClick={() => setIsOpen(!isOpen)}
+          className="h-[50px] bg-yellow-700 relative"
+        >
+          <div>Some content</div>
+          <div ref={someRef}></div>
+          {isOpen && (
+            <div
+              style={{
+                height: `calc(100vh - ${topPosition?.toString()}px - 24px)`,
+              }}
+              className={`bg-red-300 absolute top-[50px] p-[16px] left-0 right-0`}
+            >
+              <div className="bg-red-600 h-full flex flex-col p-[16px]">
+                <div>Top content</div>
+                <div className="mt-auto">Buttom content</div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
